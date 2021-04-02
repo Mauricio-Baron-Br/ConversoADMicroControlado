a6a1d5b366613a9c8558844d22506597bd41c316
Mauricio Baron <69405597+Mauricio-Baron-Br@users.noreply.github.com>
2021-04-02 19:22:45 -0300

    Impresao do LCD

--- controladorAD.c
+++ controladorAD.c
@@ -1,23 +1,48 @@
-//CAREGA A BIBLIOTECA PARA O MICROCONTROLADOR PIC16F628A
-#INCLUDE <16F628A.h>
-//CONFIGURACAO DOS FUSES
-#FUSES NOWDT
-#FUSES NOPUT
-#FUSES NOBROWNOUT
-#FUSES NOMCLR
-#FUSES NOLVP
-#FUSES NOCPD
-//DEFINE A UTILIZACAO DO CLOCK INTERNO 
-#use delay(clock=4000000)
-//PINO AO QUAL O TRANSISTOR ESTA LIGADO
-#DEFINE PINOTRANSISTOR PIN_B0
-void main(){
-  
-    while(true){
-      OUTPUT_BIT(PINOTRANSISTOR, true); //LIGA O TRANSISTOR
-      DELAY_MS(1000); //AGUARDA 1 SEGUNDO
-      OUTPUT_BIT(PINOTRANSISTOR,false); //DESLIGA O TRANSISTOR
-      DELAY_MS(1000); //AGUARDA 2 SEGUNDO
-    }
-}
+//include de bibliotecas
+#include <controladorAD.h>
+#include<stdio.h>
+#include <stdlib.h>
 
+//************************
+//definiçoes de configuraçoes de pinos
+#define LCD_ENABLE_PIN PIN_B5
+#define LCD_RS_PIN PIN_B6
+#define LCD_RW_PIN PIN_B7
+#define LCD_DATA4 PIN_B4
+#define LCD_DATA5 PIN_B3
+#define LCD_DATA6 PIN_B2
+#define LCD_DATA7 PIN_B1
+#define PIN_ATUALIZA PIN_B0
+//include de bibliotecas LCD
+#include <lcd.c>
+//*************************
+//prototipos de funçoes
+void AtualizaEstadoPino();
+//*************************
+void main()
+{
+  lcd_init();
+  output_bit(PIN_B7,false);
+  printf(lcd_putc,"\fImprimeValorA/D\n");
+  int vdados[7];
+  int i;
+  
+  while(TRUE){
+     AtualizaEstadoPino();
+     for(i=0;i<8;i++){
+         vdados[i]=i;
+     }
+     lcd_gotoxy(1,2);
+     printf(lcd_putc,"Valor:");
+     lcd_gotoxy(7,2);
+     for(i=0;i<8;i++){
+     printf(lcd_putc,"%d",vdados[i]);
+     delay_ms(500);
+     }
+   }
+}   
+   void AtualizaEstadoPino(){
+      output_bit(PIN_ATUALIZA,true);
+      delay_ms(50);
+      output_bit(PIN_ATUALIZA, false);
+}
